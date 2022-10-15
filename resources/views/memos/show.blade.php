35334a92c4415642fa2b6ee765f07b5b7b2d9194
gRaY4449 <onlyforschoolclass378@gmail.com>
2022-10-15 15:07:46 +0900

    更新・削除機能作成

--- resources/views/memos/show.blade.php
+++ resources/views/memos/show.blade.php
@@ -10,5 +10,15 @@
     <a href="/memos">戻る</a>
     <h1>{{ $memo->title }}</h1>
     <p>{!! nl2br(e($memo->body)) !!}</p>
+
+    <div class="button-group">
+        <!-- $memoのidを元に編集ページへ遷移する -->
+        <button onclick="location.href='/memos/{{ $memo->id }}/edit'">編集する</button>
+        <form action="/memos/{{ $memo->id }}" method="post">
+            @csrf
+            @method('DELETE')
+            <input type="submit" value="削除する" onclick="if(!confirm('削除しますか？')){return false};">
+        </form>
+    </div>
 </body>
 </html>
